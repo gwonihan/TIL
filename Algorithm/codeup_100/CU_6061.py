a1648f786165184736c277859878e76e695d0867
gwonihan <dghan303@gmail.com>
2022-02-26 23:53:24 +0900

    oneday_onecommit 2026

---
+++ Algorithm/codeup_100/CU_6061.py
@@ -0,0 +1,7 @@
+a,b = map(int,input().split())
+
+print(a|b)
+
+# ** 비트단위(bitwise) 연산자는,
+# ~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor),
+# <<(bitwise left shift), >>(bitwise right shift)
