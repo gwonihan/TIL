2f5f7281a7cb62ff9854dffe3473b180f2a682ba
gwonihan <dghan303@gmail.com>
2022-03-20 17:23:28 +0900

    0320

---
+++ Algorithm/BOJ/4주차/기본 수학2/boj_4153.py
@@ -0,0 +1,11 @@
+while True:
+    num_list = list(map(int,input().split()))
+    max_num = max(num_list)
+    num_list.remove(max_num)
+
+    if sum(num_list) == 0:
+        break
+    elif num_list[0] ** 2 + num_list[1] ** 2 == max_num ** 2:
+        print('right')
+    else:
+        print('wrong')
