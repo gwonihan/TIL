7813c0d349857ff4b2f055d910b10124ba4429ea
gwonihan <dghan303@gmail.com>
2022-05-02 00:28:21 +0900

    0502

--- Algorithm/BOJ/8주차/boj_11047.py
+++ Algorithm/BOJ/8주차/boj_11047.py
@@ -4,19 +4,36 @@ N, K = map(int,input().split())
 coin_list = []
 for _ in range(N):
     coin_list.append(int(input()))
-
 coin_list.reverse()
 
-while True:
-    i = 0
-    if K - coin_list[i] < 0:
-        i += 1
-    
-    elif K - coin_list[i] > 0:
-        i = i
-    
-    else:
-        print(i)
+cnt = 0
+for coin in coin_list:
+    if K >= coin:
+        cnt += K // coin
+        K = K % coin
 
-# print(coin_list)
+        if K <= 0:
+            break
+
+print(cnt)
+
+# i = 0
+# coin = 0
+# coin_list.reverse()
+
+# while True:
+
+#     if K - coin_list[i] < 0:
+#         i += 1
+    
+#     elif K - coin_list[i] > 0:
+#         K = K - coin_list[i]
+#         coin += 1
+    
+#     else:
+#         coin += 1
+#         print(coin)
+#         break
+
+ 
 
