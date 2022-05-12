582043fa8c933dbcd7e7bbb234458de4288bf270
gwonihan <dghan303@gmail.com>
2022-05-12 21:02:03 +0900

    0512

--- Algorithm/BOJ/9주차/boj_2740.py
+++ Algorithm/BOJ/9주차/boj_2740.py
@@ -1,15 +1,20 @@
 # 행렬곱셈
+
+# 행렬 A
 N, M = map(int,input().split())
 A = []
-B = []
 for _ in range(N):
     A.append(list(map(int,input().split())))
 
+# 행렬 B
 M, K = map(int,input().split())
-
+B = []
 for _ in range(M):
     B.append(list(map(int,input().split())))
 
-for i in len(A):
-    for j in len(B):
-        A[i] 
+for l in range(K):
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            cnt += A[i][j] * B[j][l]
+        print(cnt)
