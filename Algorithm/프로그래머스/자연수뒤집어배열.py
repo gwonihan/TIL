c7eb75d584ff403df0604ee9b2cfa7abbd95f514
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-05-19 09:32:56 +0900

    0519

---
+++ Algorithm/프로그래머스/자연수뒤집어배열.py
@@ -0,0 +1,8 @@
+# 자연수 뒤집어 배열로 만들기
+from re import A
+
+
+a = 12345
+a = list(str(a))
+a = list(map(int, a))
+print(A)
---
+++ Algorithm/프로그래머스/정수내림차순.py
@@ -0,0 +1,9 @@
+# 정수 내림차순으로 배치하기
+
+def solution(n):
+    li = list(str(n))
+    li.sort(reverse=True)
+    answer = ''.join(map(str,li))
+    return int(answer)
+
+solution(118372)
