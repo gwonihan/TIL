a6c6beb685fac49c87eec6e1cc7a2075a2e76eac
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-07-02 23:20:02 +0900

    0702

---
+++ Algorithm/스터디/level1/없는 숫자 더하기.py
@@ -0,0 +1,11 @@
+# 없는 숫자 더하기
+#def solution(numbers):
+#    answer = 0
+#    num_li = range(0,10)
+#    for i in num_li:
+#        if i not in numbers:
+#            answer += i
+#    return answer
+
+def solution(numbers):
+    return 45 - sum(numbers)
