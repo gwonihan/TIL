02f71e0557b7fecb78cc61244ea6bdedf106a568
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-07-15 09:20:42 +0900

    0715

--- Algorithm/스터디/백준/boj_1920.py
+++ Algorithm/스터디/백준/boj_1920.py
@@ -23,5 +23,23 @@ for i in M_li:
             end = mid - 1
     else:
         print(0)
+
+
+# 수 찾기
+import sys
+
+
+# set..을안해줘서 안됨..
+N = int(sys.stdin.readline())
+N_li = set(map(int, sys.stdin.readline().split()))
+
+M = int(sys.stdin.readline())
+M_li = list(map(int, sys.stdin.readline().split()))
+
+for i in range(M):
+    if M_li[i] in N_li:
+        print(1)
+    else:
+        print(0)
         
 
