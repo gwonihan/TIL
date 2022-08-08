89b6db7d226cd8db0a46fd4ee3ac91a79fa4bc30
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-08-08 23:49:32 +0900

    0808

--- Algorithm/스터디/level2/최솟값 만들기.py
+++ Algorithm/스터디/level2/최솟값 만들기.py
@@ -16,3 +16,16 @@ def solution(A,B):
     return answer
 
 
+# 참고풀이
+def solution(A,B):
+    answer = 0
+
+    A.sort()
+    B.sort(reverse=True)
+    
+    for a,b in zip(A,B):
+        answer += a*b
+
+    return answer
+
+
