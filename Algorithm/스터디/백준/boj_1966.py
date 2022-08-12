dfbcf56d1ce6d9f4ebc8ff6996e59e4087b8eb0e
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-08-12 12:51:24 +0900

    0812

--- Algorithm/스터디/백준/boj_1966.py
+++ Algorithm/스터디/백준/boj_1966.py
@@ -5,12 +5,25 @@ case = int(sys.stdin.readline())
 
 for _ in range(case):
     n, m = map(int,sys.stdin.readline().split())
-    d = list(map(int, sys.stdin.readline().split()))
+    imp = list(map(int, sys.stdin.readline().split()))
+    idx = list(range(len(imp)))
+    idx[m] = 'target'
 
+    # 순서
     order = 0
 
-    li = []
-    for i in range(n):
-        li.append((i, d[i]))
-    
-    print(li) 
+    while True:
+        if imp[0] == max(imp):
+            order += 1
+
+            if idx[0]=='target':
+                print(order)
+                break
+            else:
+                imp.pop(0)
+                idx.pop(0)
+        
+        else:
+            imp.append(imp.pop(0))
+            idx.append(idx.pop(0))
+           
