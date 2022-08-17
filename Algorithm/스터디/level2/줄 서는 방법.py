dd6fdb71644ad16b0946a318eaca3d28cd418e16
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-08-17 22:17:50 +0900

    0817

---
+++ Algorithm/스터디/level2/줄 서는 방법.py
@@ -0,0 +1,19 @@
+# 줄 서는 방법
+
+import math
+
+def solution(n, k):
+    lst = [x for x in range(1, n+1)]
+    answer = []
+    k -= 1
+    
+    for i in range(n, 0, -1):
+        max_num = math.factorial(n)
+        split_num = max_num // n
+
+        print(lst[k//split_num])
+        answer.append(lst[k//split_num])
+        lst.pop(k//split_num)
+        k %= split_num
+        n -= 1
+    return answer
