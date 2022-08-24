69f3fd0d829351c2d280beef094f4118814a9271
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-08-24 23:47:01 +0900

    0824

---
+++ Algorithm/스터디/level2/숫자 블록.py
@@ -0,0 +1,14 @@
+def solution(begin, end):
+    answer = []
+    
+    li = [0] * 1000000000
+    
+    print(li)
+    # for i in range(begin, end):
+    #     if i == 1:
+    #         li[i:] = i
+        # elif i % 2 == 1:
+        #     pass
+    
+        
+    return answer
