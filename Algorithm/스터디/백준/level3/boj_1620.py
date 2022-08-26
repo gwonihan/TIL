fea2a6f624d43fc3ec4dfce221cd17020eb00d7c
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-08-26 20:04:08 +0900

    0826

--- Algorithm/스터디/백준/level3/boj_1620.py
+++ Algorithm/스터디/백준/level3/boj_1620.py
@@ -2,16 +2,38 @@
 import sys
 M,N = map(int,sys.stdin.readline().split())
 
-poketmon = []
+poketmon = {}
+for i in range(1,M+1):
+    poketmon[i] = sys.stdin.readline()
 
-for _ in range(M):
-    poketmon.append(sys.stdin.readline())
+covert_po = {v:k for k,v in poketmon.items()}
 
 
 for i in range(N):
     answer = sys.stdin.readline()
 
-    try:
-        print((poketmon.index(answer))+1)
-    except:
-        print(poketmon[int(answer)-1])
+    if answer.isdigit():
+        print(poketmon[int(answer)])
+    else:
+        print(covert_po[answer])
+        
+
+
+
+
+# 시간초과
+# M,N = map(int,sys.stdin.readline().split())
+
+# poketmon = []
+
+# for _ in range(M):
+#     poketmon.append(sys.stdin.readline())
+
+
+# for i in range(N):
+#     answer = sys.stdin.readline()
+
+#     try:
+#         print((poketmon.index(answer))+1)
+#     except:
+#         print(poketmon[int(answer)-1])
