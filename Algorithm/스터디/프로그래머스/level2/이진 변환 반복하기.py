369a6dcb824947d66b173ee8c228f8b4428ec384
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-09-16 12:58:21 +0900

    0916

--- Algorithm/스터디/프로그래머스/level2/이진 변환 반복하기.py
+++ Algorithm/스터디/프로그래머스/level2/이진 변환 반복하기.py
@@ -1,24 +1,33 @@
 def solution(s):
     answer = []
+    
+    # zero : 0의 갯수, r : 회차, length : 2진변환할 수
     zero = 0
     r = 0
     length = 0
     
+    # 최초로 주어진 s가 1이 될 때까지 반복하는 while문
     while len(s) != 1:
         for i in s:
-            if i == 0:
+            if int(i) == 0:
                 zero += 1
             else:
                 length += 1
         
+        # 0 제거 후 s의 길이를 이진 변환하는 while문
         new = ''
-        while length != 0:
+        while True:
             new += str(length%2)
             length = length // 2
+            
+            if length == 0:
+                break
         
+        # 회차 카운트와 이진변환 완료된 new를 s에 할당 
         r += 1
-        s = new
+        s = str(new)[::-1]
     
+    # 모든 while문이 종료되면, r(회차) 와 zero(0의 개수)를 answer에 append
     answer.append(r)
     answer.append(zero)
       
