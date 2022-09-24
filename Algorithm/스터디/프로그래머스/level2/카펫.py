3598008cb4e06cbc32deb8d5458ab4a3144330a5
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-09-24 21:16:54 +0900

    0924

--- Algorithm/스터디/프로그래머스/level2/카펫.py
+++ Algorithm/스터디/프로그래머스/level2/카펫.py
@@ -1,7 +1,35 @@
 #카펫
 def solution(brown, yellow):
     answer = []
+    total = brown + yellow                  # a * b = total
+    for b in range(1,total+1):
+        if (total / b) % 1 == 0:            # total / b = a
+            a = total / b
+            if a >= b:                      # a >= b
+                if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
+                    return [a,b]
+            
+    return answer
+
+
+# 코드실행은 됬으나 실패
+def solution(brown, yellow):
+    answer = []
     
-
-
+    import math
+    total_grid = brown + yellow
+    root = total_grid ** (1/2)
+    
+    width = math.ceil(root)
+    vertical = math.floor(root)
+    
+    for i in range(width, total_grid+1):
+        if len(answer) == 0:
+            for j in range(vertical+1, 0,-1):
+                if i * j == total_grid:
+                    answer.append(i)
+                    answer.append(j)
+                    break
+        else:
+            break
     return answer
