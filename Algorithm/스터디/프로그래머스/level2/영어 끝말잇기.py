cb9cff97db5d0d50059c6571de715d9fd390706e
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-09-29 10:26:32 +0900

    p_영어끝끝말잇기solve

--- Algorithm/스터디/프로그래머스/level2/영어 끝말잇기.py
+++ Algorithm/스터디/프로그래머스/level2/영어 끝말잇기.py
@@ -1,11 +1,23 @@
 # 영어끝말잇기
 def solution(n, words):
     answer = []
+    # 끝말 잇기 단어 리스트
+    c_word = []
+    c_word.append(words[0])
     
-    for j in range((len(words)-1)):
-        if words[j][-1] != words[j+1][0]:
-            answer.append(j//n)
-            answer.append(j%n)
+    for i in range(len(words)-1):
+        # 다음에 나올 단어가 c_word 에 없다면
+        if words[i+1] not in c_word:
+            
+            if words[i][-1] != words[i+1][0]:
+                # 사람 순서, 회차
+                answer = [(i+1)%n+1,(i+1)//n+1]
+                break
+            else:
+                c_word.append(words[i+1])
+        else:
+            answer = [(i+1)%n+1,(i+1)//n+1]
             break
-   
+    else:
+        answer = [0,0]     
     return answer
