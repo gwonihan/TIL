25b48d1b2d4cf2e859258cb36542a2bf71aa700c
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-10-03 22:26:32 +0900

    boj_9095 solved

--- Algorithm/스터디/백준/level3/boj_9095.py
+++ Algorithm/스터디/백준/level3/boj_9095.py
@@ -1,5 +1,15 @@
 # 1,2,3 더하기
 n = int(input())
 
+d=[0]*11
+d[1]=1
+d[2]=2
+d[3]=4
+
+for i in range(4,11):
+    d[i] = d[i-3]+d[i-2]+d[i-1]
+
 for _ in range(n):
+    m = int(input())
+    print(d[m])
     
