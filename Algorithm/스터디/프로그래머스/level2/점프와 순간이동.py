1c6b35785a2cd93903ec9ce7024deb3f4ddda285
Donggwon Han <87125245+gwonihan@users.noreply.github.com>
2022-10-18 12:43:40 +0900

    p_점프와순간이동solved

---
+++ Algorithm/스터디/프로그래머스/level2/점프와 순간이동.py
@@ -0,0 +1,12 @@
+# 점프와 순간이동
+def solution(n):
+    ans = 0
+    
+    while n != 0:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n -= 1
+            ans += 1
+        
+    return ans
