dd673f5f1321e29a46de89e8ef0bda0a376505ae
gwonihan <dghan303@gmail.com>
2022-11-02 20:21:15 +0900

    1102 괄호 회전하기

--- Algorithm/스터디/프로그래머스/level2/괄호 회전하기.py
+++ Algorithm/스터디/프로그래머스/level2/괄호 회전하기.py
@@ -1,1 +1,28 @@
 # 괄호 회전하기
+def solution(s):
+    answer = 0
+    
+    for _ in range(len(s)):
+        st = []
+        
+        for i in range(len(s)):
+            
+            if len(st) > 0:
+                if st[-1] == '[' and s[i] == ']':
+                    st.pop()
+                elif st[-1] == '(' and s[i] == ')':
+                    st.pop()
+                elif st[-1] == '{' and s[i] == '}':
+                    st.pop()
+                else:
+                    st.append(s[i])
+            else:
+                st.append(s[i])
+        
+        if len(st) == 0:
+            answer += 1
+        a = s[0]
+        s = s[1:]
+        s += a
+    
+    return answer
