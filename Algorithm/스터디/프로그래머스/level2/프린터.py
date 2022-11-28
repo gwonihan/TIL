57404f35ac456128b0ad9dd06756aea9dd954563
gwonihan <dghan303@gmail.com>
2022-11-28 20:24:01 +0900

    1128

---
+++ Algorithm/스터디/프로그래머스/level2/프린터.py
@@ -0,0 +1,8 @@
+# 프린터
+def solution(priorities, location):                             
+    answer = 0
+    li = []
+    for i in enumerate(priorities):
+        li.append(i)
+    
+    return answer
