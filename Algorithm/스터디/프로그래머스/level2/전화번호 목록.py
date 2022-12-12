ab7ff2914024f53ce81fb377394e74ca1c5f8ab2
gwonihan <dghan303@gmail.com>
2022-12-12 22:12:02 +0900

    p_전화번호풀이

--- Algorithm/스터디/프로그래머스/level2/전화번호 목록.py
+++ Algorithm/스터디/프로그래머스/level2/전화번호 목록.py
@@ -1,4 +1,13 @@
 def solution(phone_book):
+    phone_book.sort()
+    
+    for i, j in zip(phone_book, phone_book[1:]):
+        if j.startswith(i):
+            return False
+    return True
+
+# 틀린 풀이
+def solution(phone_book):
     
     for i in range(len(phone_book)):
         for j in range(i+1, len(phone_book)):
