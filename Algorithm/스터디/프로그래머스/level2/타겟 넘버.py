98f6839aad407203b25f962198268bb05738c510
gwonihan <dghan303@gmail.com>
2023-02-16 10:12:15 +0900

    주식가격

--- Algorithm/스터디/프로그래머스/level2/타겟 넘버.py
+++ Algorithm/스터디/프로그래머스/level2/타겟 넘버.py
@@ -1,10 +1,1 @@
-def recursive(numbers, sum, target):
-    if numbers == []:
-        if sum == target:
-            return 1
-        else:
-            return 0
-    return recursive(numbers[1:], sum + numbers[0], target) + recursive(numbers[1:], sum - numbers[0], target)
-
-def solution(numbers, target):
-    return recursive(numbers, 0, target)
+# 타겟 넘버
