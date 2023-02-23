a0c78a6ccf997e5da180e21bf583d6cca42c0f85
gwonihan <dghan303@gmail.com>
2023-02-23 19:02:36 +0900

    0223

---
+++ Algorithm/스터디/프로그래머스/level2/[1차] 프렌즈4블록.py
@@ -0,0 +1,38 @@
+def solution(m, n, board):
+    for i in range(m):
+        board[i] = list(board[i])
+        
+    cnt = 0
+    rm = set()
+    
+    while True:
+        for i in range(m-1):
+            for j in range(n-1):
+                t = board[i][j]
+                
+                if t == []:
+                    continue
+                if board[i+1][j] == t and board[i][j+1] == t and board[i+1][j+1] == t:
+                    rm.add((i, j))
+                    rm.add((i+1, j))
+                    rm.add((i, j+1))
+                    rm.add((i+1, j+1))
+        
+        if rm:
+            cnt += len(rm)
+            for i, j in rm:
+                board[i][j] = []
+            rm = set()
+        else:
+            return cnt
+        
+        while True:
+            moved = 0
+            for i in range(m-1):
+                for j in range(n):
+                    if board[i][j] and board[i+1][j] == []:
+                        board[i+1][j] = board[i][j]
+                        board[i][j] = []
+                        moved = 1
+            if moved == 0:
+                break
