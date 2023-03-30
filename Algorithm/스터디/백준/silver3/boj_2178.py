5027a3e9b6fbb1b1ba14829994f666cd19e60e97
gwonihan <dghan303@gmail.com>
2023-03-30 14:47:18 +0900

    0330

---
+++ Algorithm/스터디/백준/silver3/boj_2178.py
@@ -0,0 +1,33 @@
+# 미로 탐색
+import sys
+from collections import deque
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    # readline의 경우 맨 뒤 '\n' 까지 입력받으므로 제거
+    graph.append(list(map(int, input().rstrip())))
+
+# 상하좌우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+    
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
